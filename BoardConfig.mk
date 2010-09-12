4b4984afab826db5c52902b371679ba613b2a4ec
noobnl <shareefalis@gmail.com>
2010-09-12 01:52:07 -0500

    epic 4g port

---
+++ BoardConfig.mk
@@ -0,0 +1,82 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# config.mk
+#
+# Product-specific compile-time definitions.
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
+-include vendor/samsung/epic/BoardConfigVendor.mk
+
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RECOVERY := true
+TARGET_NO_KERNEL := true
+
+TARGET_BOARD_PLATFORM := s5pc110
+TARGET_BOARD_PLATFORM_GPU := POWERVR_SGX540_120
+
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+
+BOARD_USES_GENERIC_AUDIO := true
+
+BOARD_HAVE_BLUETOOTH := true
+BT_USE_BTL_IF := true
+BT_ALT_STACK := true
+
+BRCM_BTL_INCLUDE_A2DP := true
+BRCM_BT_USE_BTL_IF := true
+
+BOARD_EGL_CFG := device/samsung/epic/prebuilt/egl/egl.cfg
+
+BOARD_KERNEL_CMDLINE := no_console_suspend=1 console=null
+BOARD_KERNEL_BASE := 0x02e00000
+
+BOARD_BOOTIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x00280000)
+BOARD_RECOVERYIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x00500000)
+BOARD_SYSTEMIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x07500000)
+BOARD_USERDATAIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x04ac0000)
+# The size of a block that can be marked bad.
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_USES_FFORMAT := true
+BOARD_RECOVERY_IGNORE_BOOTABLES := true
+
+BOARD_DATA_DEVICE := /dev/block/stl10
+BOARD_DATA_FILESYSTEM := rfs
+BOARD_DATA_FILESYSTEM_OPTIONS := llw,check=no,nosuid,nodev
+## doesn't have dbdata partition
+##BOARD_HAS_DATADATA := true
+##BOARD_DATADATA_DEVICE := /dev/block/stl10
+##BOARD_DATADATA_FILESYSTEM := rfs
+##BOARD_DATADATA_FILESYSTEM_OPTIONS := llw,check=no,nosuid,nodev
+BOARD_SYSTEM_DEVICE := /dev/block/stl9
+BOARD_SYSTEM_FILESYSTEM := rfs
+BOARD_SYSTEM_FILESYSTEM_OPTIONS := llw,check=no
+BOARD_CACHE_DEVICE := /dev/block/stl11
+BOARD_CACHE_FILESYSTEM := rfs
+BOARD_CACHE_FILESYSTEM_OPTIONS := llw,check=no,nosuid,nodev
+BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk0
+BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk0p1
+BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
+TARGET_PREBUILT_KERNEL := device/samsung/epic/kernel
---
+++ full_epic.mk
@@ -0,0 +1,178 @@
+#
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Discard inherited values and use our own instead.
+PRODUCT_NAME := Samsung Epic 4G
+PRODUCT_DEVICE := epic
+PRODUCT_MODEL := SPH-D700
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := Samsung
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+
+PRODUCT_COPY_FILES += \
+    device/samsung/epic/init.smdkc110.rc:root/init.smdkc110.rc
+
+PRODUCT_POLICY := android.policy_phone
+
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=240 \
+    rild.libpath=/system/lib/libsec-ril40.so \
+    rild.libargs=-d /dev/ttyS0 \
+    wifi.interface=eth0 \
+    wifi.supplicant_scan_interval=15
+
+
+
+# Install the features available on this device.
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml
+
+#
+# Copy epic specific prebuilt files
+#
+
+#
+# Wifi
+#
+PRODUCT_COPY_FILES += \
+    device/samsung/epic/prebuilt/wifi/libwlservice.so:system/lib/libwlservice.so \
+    device/samsung/epic/prebuilt/wifi/nvram_net.txt:system/etc/wifi/nvram_net.txt \
+    device/samsung/epic/prebuilt/wifi/nvram_mfg.txt:system/etc/wifi/nvram_mfg.txt \
+    device/samsung/epic/prebuilt/wifi/bcm4329_aps.bin:system/etc/wifi/bcm4329_aps.bin \
+    device/samsung/epic/prebuilt/wifi/bcm4329_mfg.bin:system/etc/wifi/bcm4329_mfg.bin \
+    device/samsung/epic/prebuilt/wifi/bcm4329_sta.bin:system/etc/wifi/bcm4329_sta.bin \
+    device/samsung/epic/prebuilt/wifi/wifi.conf:system/etc/wifi/wifi.conf \
+    device/samsung/epic/prebuilt/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+    device/samsung/epic/prebuilt/wifi/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
+    device/samsung/epic/prebuilt/wifi/wlservice:system/bin/wlservice \
+    device/samsung/epic/prebuilt/wifi/wpa_supplicant:system/bin/wpa_supplicant
+
+
+
+#
+# Display (3D)
+#
+PRODUCT_COPY_FILES += \
+    device/samsung/epic/prebuilt/egl/egl.cfg:system/lib/egl/egl.cfg \
+    device/samsung/epic/prebuilt/egl/libEGL_POWERVR_SGX540_120.so:system/lib/egl/libEGL_POWERVR_SGX540_120.so \
+    device/samsung/epic/prebuilt/egl/libGLES_android.so:system/lib/egl/libGLES_android.so \
+    device/samsung/epic/prebuilt/egl/libGLESv1_CM_POWERVR_SGX540_120.so:system/lib/egl/libGLESv1_CM_POWERVR_SGX540_120.so \
+    device/samsung/epic/prebuilt/egl/libGLESv2_POWERVR_SGX540_120.so:system/lib/egl/libGLESv2_POWERVR_SGX540_120.so
+
+#
+# Sensors, Lights etc
+#
+PRODUCT_COPY_FILES += \
+    device/samsung/epic/prebuilt/hw/copybit.s5pc110.so:system/lib/hw/copybit.s5pc110.so \
+    device/samsung/epic/prebuilt/hw/lights.default.so:system/lib/hw/lights.default.so \
+    device/samsung/epic/prebuilt/hw/sensors.SPH-D700.so:system/lib/hw/sensors.SPH-D700.so \
+    device/samsung/epic/prebuilt/hw/gralloc.s5pc110.so:system/lib/hw/gralloc.s5pc110.so
+
+#
+# Keys
+#
+PRODUCT_COPY_FILES += \
+    device/samsung/epic/prebuilt/keylayout/s3c-keypad.kl:system/usr/keylayout/s3c-keypad.kl \
+    device/samsung/epic/prebuilt/keylayout/sec_jack.kl:system/usr/keylayout/sec_jack.kl \
+    device/samsung/epic/prebuilt/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
+    device/samsung/epic/prebuilt/keylayout/melfas-touchkey.kl:system/usr/keylayout/melfas-touchkey.kl \
+    device/samsung/epic/prebuilt/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
+    device/samsung/epic/prebuilt/keychars/s3c-keypad.kcm.bin:system/usr/keychars/s3c-keypad.kcm.bin \
+    device/samsung/epic/prebuilt/keychars/sec_jack.kcm.bin:system/usr/keychars/sec_jack.kcm.bin \
+    device/samsung/epic/prebuilt/keychars/melfas-touchkey.kcm.bin:system/usr/keychars/melfas-touchkey.kcm.bin \
+    device/samsung/epic/prebuilt/keychars/qwerty.kcm.bin:system/usr/keychars/qwerty.kcm.bin \
+    device/samsung/epic/prebuilt/keychars/s3c-keypad-rev0050.kcm.bin:system/usr/keychars/s3c-keypad-rev0050.kcm.bin \
+    device/samsung/epic/prebuilt/keychars/s3c-keypad-rev0040.kcm.bin:system/usr/keychars/s3c-keypad-rev0040.kcm.bin \
+	device/samsung/epic/prebuilt/keychars/s3c-keypad-rev0030.kcm.bin:system/usr/keychars/s3c-keypad-rev0030.kcm.bin \
+	device/samsung/epic/prebuilt/keychars/s3c-keypad-rev0000.kcm.bin:system/usr/keychars/s3c-keypad-rev0000.kcm.bin \
+	device/samsung/epic/prebuilt/keychars/melfas_ts_input.kcm.bin:system/usr/keychars/melfas_ts_input.kcm.bin \
+	device/samsung/epic/prebuilt/keychars/optjoy_device.kcm.bin:system/usr/keychars/optjoy_device.kcm.bin \
+    device/samsung/epic/prebuilt/keychars/qwerty2.kcm.bin:system/usr/keychars/qwerty2.kcm.bin  
+
+
+#
+# Vold
+#
+PRODUCT_COPY_FILES += \
+    device/samsung/epic/prebuilt/vold.fstab:system/etc/vold.fstab 
+
+#
+# Camera
+#
+PRODUCT_COPY_FILES += \
+    device/samsung/epic/prebuilt/camera/libarccamera.so:system/lib/libarccamera.so \
+    device/samsung/epic/prebuilt/camera/libcamerafirmwarejni.so:system/lib/libcamerafirmwarejni.so \
+    device/samsung/epic/prebuilt/camera/libcamera.so:system/lib/libcamera.so \
+    device/samsung/epic/prebuilt/camera/libseccameraadaptor.so:system/lib/libseccameraadaptor.so \
+    device/samsung/epic/prebuilt/camera/libseccamera.so:system/lib/libseccamera.so
+
+#
+# RIL
+#
+PRODUCT_COPY_FILES += \
+    device/samsung/epic/prebuilt/ril/libril.so:system/lib/libril.so \
+    device/samsung/epic/prebuilt/ril/libsec-ril40.so:system/lib/libsec-ril40.so \
+    device/samsung/epic/prebuilt/ril/libsecril-client.so:system/lib/libsecril-client.so \
+    device/samsung/epic/prebuilt/ril/drexe:system/bin/drexe \
+    device/samsung/epic/prebuilt/ril/libsec-ril40.so:system/lib/librefence.so \
+    device/samsung/epic/prebuilt/ril/rilclient-test:system/bin/rilclient-test
+	
+
+#
+# OMX
+#
+PRODUCT_COPY_FILES += \
+    device/samsung/epic/prebuilt/omx/libs263domxoc.so:system/lib/libs263domxoc.so \
+    device/samsung/epic/prebuilt/omx/libs263eomxoc.so:system/lib/libs263eomxoc.so \
+    device/samsung/epic/prebuilt/omx/libs264domxoc.so:system/lib/libs264domxoc.so \
+    device/samsung/epic/prebuilt/omx/libs264eomxoc.so:system/lib/libs264eomxoc.so \
+    device/samsung/epic/prebuilt/omx/libsaacdomxoc.so:system/lib/libsaacdomxoc.so \
+    device/samsung/epic/prebuilt/omx/libsaaceomxoc.so:system/lib/libsaaceomxoc.so \
+    device/samsung/epic/prebuilt/omx/libsac3domxoc.so:system/lib/libsac3domxoc.so \
+    device/samsung/epic/prebuilt/omx/libsamrdomxoc.so:system/lib/libsamrdomxoc.so \
+    device/samsung/epic/prebuilt/omx/libsamreomxoc.so:system/lib/libsamreomxoc.so \
+    device/samsung/epic/prebuilt/omx/libsdiv3domxoc.so:system/lib/libsdiv3domxoc.so \
+    device/samsung/epic/prebuilt/omx/libsflacdomxoc.so:system/lib/libsflacdomxoc.so \
+    device/samsung/epic/prebuilt/omx/libsmp3domxoc.so:system/lib/libsmp3domxoc.so \
+    device/samsung/epic/prebuilt/omx/libsmp4fmocn.so:system/lib/libsmp4fmocn.so \
+    device/samsung/epic/prebuilt/omx/libsmp4vdomxoc.so:system/lib/libsmp4vdomxoc.so \
+    device/samsung/epic/prebuilt/omx/libsmp4veomxoc.so:system/lib/libsmp4veomxoc.so \
+    device/samsung/epic/prebuilt/omx/libsvc1domxoc.so:system/lib/libsvc1domxoc.so \
+    device/samsung/epic/prebuilt/omx/libswmadomxoc.so:system/lib/libswmadomxoc.so \
+    device/samsung/epic/prebuilt/omx/libswmv7domxoc.so:system/lib/libswmv7domxoc.so \
+    device/samsung/epic/prebuilt/omx/libswmv8domxoc.so:system/lib/libswmv8domxoc.so \
+
+# libspeech
+PRODUCT_COPY_FILES += device/samsung/epic/prebuilt/audio/libspeech.so:system/lib/libspeech.so
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+LOCAL_KERNEL := device/samsung/epic/kernel
+else
+LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+# we have enough storage space to hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.startheapsize=8m \
+    dalvik.vm.heapsize=48m
+
+# epic uses high-density artwork where available
+PRODUCT_LOCALES := hdpi
+
+
+
